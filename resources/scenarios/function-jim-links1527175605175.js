1d99acc0c304c58af881d9330c755a1b2945d354
Laican1996 <albertomendozacasillas@gmail.com>
2018-06-28 14:10:14 -0500

    a ver si jala

---
+++ resources/scenarios/function-jim-links1527175605175.js
@@ -0,0 +1,12 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Button_1" : [
+        "96e55e0c-532f-47ec-b9fa-3f0672249a11"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
